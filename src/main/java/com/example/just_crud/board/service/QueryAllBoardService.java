6c0b55adcd90e9d6a275f2d77880b2b9e8236d32
troy96475 <taesupark0504@gmail.com>
2024-12-29 01:49:42 +0900

    feat :: 전체 조회

--- src/main/java/com/example/just_crud/board/service/QueryAllBoardService.java
+++ src/main/java/com/example/just_crud/board/service/QueryAllBoardService.java
@@ -1,7 +1,7 @@
-package com.example.crud_practice.board.service;
+package com.example.just_crud.board.service;
 
-import com.example.crud_practice.board.entity.Board;
-import com.example.crud_practice.board.repository.BoardRepository;
+import com.example.just_crud.board.entity.Board;
+import com.example.just_crud.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
