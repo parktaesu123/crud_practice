ae4df15c8e03d4c5095de23efdcac36245720d65
troy96475 <taesupark0504@gmail.com>
2024-12-29 01:35:39 +0900

    first commit

---
+++ src/main/java/com/example/just_crud/JustCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.just_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/just_crud/JustCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.just_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
