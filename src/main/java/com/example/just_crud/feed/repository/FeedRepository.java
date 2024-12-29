4baf825b9749c7078b9ff66d82bee8b6b5abc03b
troy96475 <taesupark0504@gmail.com>
2024-12-29 19:50:26 +0900

    refactor :: dao 수정

--- src/main/java/com/example/just_crud/feed/repository/FeedRepository.java
+++ src/main/java/com/example/just_crud/feed/repository/FeedRepository.java
@@ -4,6 +4,13 @@ import com.example.just_crud.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+    Optional<Feed> findById(Long id);
+
+    Optional<Feed> findByTitle(String title);
 }
