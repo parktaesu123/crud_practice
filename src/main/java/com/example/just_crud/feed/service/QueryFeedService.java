5eb0406b40d634364a00a36c56902fb94bc911c2
troy96475 <taesupark0504@gmail.com>
2024-12-29 19:49:29 +0900

    feat :: 게시글 조회

--- src/main/java/com/example/just_crud/feed/service/QueryFeedService.java
+++ src/main/java/com/example/just_crud/feed/service/QueryFeedService.java
@@ -1,7 +1,6 @@
 package com.example.just_crud.feed.service;
 
 import com.example.just_crud.feed.dto.response.FeedResponse;
-import com.example.just_crud.feed.entity.Feed;
 import com.example.just_crud.feed.repository.FeedRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
