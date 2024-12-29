4588d36fddffdccf88e834dc1238979a2aa9d903
troy96475 <taesupark0504@gmail.com>
2024-12-29 19:49:43 +0900

    feat :: 게시글 전체 조회

--- src/main/java/com/example/just_crud/feed/service/QueryAllFeedService.java
+++ src/main/java/com/example/just_crud/feed/service/QueryAllFeedService.java
@@ -1,7 +1,25 @@
-package com.example.crud_practice.feed.service;
+package com.example.just_crud.feed.service;
 
+import com.example.just_crud.feed.dto.response.FeedResponse;
+import com.example.just_crud.feed.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class QueryAllFeedService {
+    private final FeedRepository feedRepository;
+
+    public List<FeedResponse> queryAllFeed() {
+        List<FeedResponse> feedResponses = feedRepository.findAll()
+                .stream()
+                .map(feed -> new FeedResponse(feed))
+                .collect(Collectors.toList());
+
+        return feedResponses;
+    }
+
 }
