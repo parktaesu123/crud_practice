c26f1d1074882c3b451171cad021676c67c67bf9
troy96475 <taesupark0504@gmail.com>
2024-12-29 15:48:15 +0900

    feat :: 게시글 조회

--- src/main/java/com/example/just_crud/feed/service/QueryFeedService.java
+++ src/main/java/com/example/just_crud/feed/service/QueryFeedService.java
@@ -1,2 +1,20 @@
-package com.example.just_crud.feed.service;public class QueryFeedService {
+package com.example.just_crud.feed.service;
+
+import com.example.just_crud.feed.dto.response.FeedResponse;
+import com.example.just_crud.feed.entity.Feed;
+import com.example.just_crud.feed.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryFeedService {
+    private final FeedRepository feedRepository;
+
+    public FeedResponse queryFeed(Long id) {
+        return feedRepository.findById(id)
+                .map(feed -> new FeedResponse(feed)) //optional<FeedResponse>로 변환
+                .orElseThrow(() -> new RuntimeException("존재하지 않은 글입니다."));
+
+    }
 }
