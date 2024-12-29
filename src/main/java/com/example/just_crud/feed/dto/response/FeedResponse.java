ffe1102787c53f3ca6d056ea24fe4a2b6741ede3
troy96475 <taesupark0504@gmail.com>
2024-12-29 19:50:12 +0900

    refactor :: dto 수정

--- src/main/java/com/example/just_crud/feed/dto/response/FeedResponse.java
+++ src/main/java/com/example/just_crud/feed/dto/response/FeedResponse.java
@@ -2,6 +2,7 @@ package com.example.just_crud.feed.dto.response;
 
 import com.example.just_crud.feed.entity.Feed;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,8 @@ public class FeedResponse {
     private String content;
     private String userName;
 
+
+    @Builder
     public FeedResponse(Feed feed) {
         this.id = feed.getId();
         this.title = feed.getTitle();
