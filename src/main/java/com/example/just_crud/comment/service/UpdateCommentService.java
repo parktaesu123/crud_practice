9bddc3eae5055b489f831a20603af0157f30573a
troy96475 <taesupark0504@gmail.com>
2024-12-31 00:13:07 +0900

    feat :: 수정

---
+++ src/main/java/com/example/just_crud/comment/service/UpdateCommentService.java
@@ -0,0 +1,22 @@
+package com.example.just_crud.comment.service;
+
+import com.example.just_crud.comment.dto.request.CommentRequest;
+import com.example.just_crud.comment.entity.Comment;
+import com.example.just_crud.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateCommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public void updateComment(CommentRequest request, Long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("댓글 없음"));
+
+        comment.update(request.getContent());
+    }
+}
